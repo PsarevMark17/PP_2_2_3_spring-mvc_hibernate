e57eecf4f9302f0184921509d880c3dbf675e9f8
PsarevMark17 <psarevmark17@protonmail.com>
2024-07-08 17:39:54 +0300

    Meow-meow-meow 2

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -1,14 +1,12 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import web.model.User;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String users(Model model) {
+    public String users() {
         return "/";
     }
 }
